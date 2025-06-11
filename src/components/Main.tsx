6fa2ea2c9e948bfe28d2151ae39ad4a7811d3def
PJaramilloV <pablojaramillovesperinas@gmail.com>
2025-06-11 18:34:46 -0400

    Changed identifiers

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,19 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="https://purepng.com/public/uploads/large/31508450097pvfewr0ivkfpo5tyz6do1gcgnwevbei6hcaromkvzral2l2yuzl0dxharwhya5ravvbn6aqdh1xi6mhp1ecbwdtknnzbhq9qqisy.png" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/pjaramillov" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/pablo-jaramillo-vesperinas/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Pablo Jaramillo</h1>
+          <p>ML Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/pjaramillov" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/pablo-jaramillo-vesperinas/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
