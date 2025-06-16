5f21d4f53d0c4e485907433a75f1d9003c874873
PJaramilloV <pablojaramillovesperinas@gmail.com>
2025-06-16 18:12:36 -0400

    feat: added video support to project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,20 @@
 import '../assets/styles/Project.scss';
+import ReactPlayer from 'react-player'
 
 interface ProjectItems {
     name: string,
     url: string,
-    imgPath: string,
+    img_or_vid_url: string,
     description: string
 }
 
-function Project({name, url, imgPath, description}: ProjectItems){
+function Project({name, url, img_or_vid_url, description}: ProjectItems){
     return <div className="project">
-                <a href={url} target="_blank" rel="noreferrer"><img src={imgPath} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href={url} target="_blank" rel="noreferrer">
+                    { img_or_vid_url.includes(".mp4") ? 
+                        <ReactPlayer url={img_or_vid_url} playing={true} loop={true} width={800} height={485} /> : 
+                        <img src={img_or_vid_url} className="zoom" alt="thumbnail" width="100%"/>}
+                </a>
                 <a href={url} target="_blank" rel="noreferrer"><h2>{name}</h2></a>
                 <p>{description}</p>
             </div>
