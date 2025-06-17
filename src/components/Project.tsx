ea3657493ff131ad6d9255bfd78a2f0050620433
PJaramilloV <pablojaramillovesperinas@gmail.com>
2025-06-17 10:56:51 -0400

    fix: fixed video size now dynamic to be responsive to display

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,7 +12,7 @@ function Project({name, url, img_or_vid_url, description}: ProjectItems){
     return <div className="project">
                 <a href={url} target="_blank" rel="noreferrer">
                     { img_or_vid_url.includes(".mp4") ? 
-                        <ReactPlayer url={img_or_vid_url} playing={true} loop={true} volume={0} width={800} height={485} /> : 
+                        <ReactPlayer url={img_or_vid_url} playing={true} loop={true} volume={0} width={"100%"} height={"50%"} /> : 
                         <img src={img_or_vid_url} className="zoom" alt="thumbnail" width="100%"/>}
                 </a>
                 <a href={url} target="_blank" rel="noreferrer"><h2>{name}</h2></a>
